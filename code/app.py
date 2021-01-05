6ab55de793aa37bbdddf14bb9af0cd64054bfb48
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-01-05 12:23:12 +0100

    Added first endpoints for items Api

--- code/app.py
+++ code/app.py
@@ -4,10 +4,25 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item, 200
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}, 200
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
 
 app.run(port=5000)
