b3c40001a8da0e4511e985a80766da898fa50cf0
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-01-28 14:34:23 +0100

    refactored insertion of items into database

--- code/item.py
+++ code/item.py
@@ -37,6 +37,15 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
 
+        try:
+            self.insert(item)
+        except:
+            return {'message': 'An error occurred inserting the item.'}, 500
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -45,7 +54,7 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-        return item, 201
+        
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
