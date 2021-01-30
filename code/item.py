df309d3959c3343b7a85e8262f9763e79c3382bf
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-01-30 18:23:19 +0100

    Added ItemList database functionality

--- code/item.py
+++ code/item.py
@@ -99,5 +99,16 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items': items}, 200
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
+        return {'items': items}
 
