5ea52c687e0660d81a30b5370cf730e9aba3d757
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-02-08 13:59:09 +0100

    Added store resource

--- code/app.py
+++ code/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 from db import db
 
@@ -21,7 +22,9 @@ def create_tables():
 jwt = JWT(app, authenticate, identity)
 
 
+api.add_resourcce(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
